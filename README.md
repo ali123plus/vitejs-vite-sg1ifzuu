f6ee3f5c4337b995a38dcf70f8d7d6a19df10f94
ali123plus <ali123plus@gmail.com>
2023-11-27 21:58:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-sg1ifzuu
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/ali123plus/vitejs-vite-sg1ifzuu)
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+console.log(z.coerce.boolean().parse("tuna"));
+
+z.coerce.boolean().parse("tuna"); // => true
+z.coerce.boolean().parse("true"); // => true
+z.coerce.boolean().parse("false"); // => true
+z.coerce.boolean().parse(1); // => true
+z.coerce.boolean().parse([]); // => true
+
+z.coerce.boolean().parse(0); // => false
+z.coerce.boolean().parse(undefined); // => false
+z.coerce.boolean().parse(null); // => false
+
